823c55db08749510b0a34ec240dc8cf8d0fb84a1
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-08 18:10:29 -0400

    ready for review

--- src/main/java/hbcu/stay/ready/mastering_loops/TableUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TableUtilities.java
@@ -1,15 +1,37 @@
 package hbcu.stay.ready.mastering_loops;
 
 public class TableUtilities {
+
     public static String getSmallMultiplicationTable() {
-        return null;
+        String result = "";
+        for(int col = 1; col <= 5; col++){
+            for(int row = 1; row <= 5; row++){
+                result += String.format("%3d |", col * row);
+            }
+            result += "\n";
+        }
+        return result;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        String result = "";
+        for(int col = 1; col <= 10; col++){
+            for(int row = 1; row <= 10; row++){
+                result += String.format("%3d |", col * row);
+            }
+            result += "\n";
+        }
+        return result;
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+        String result = "";
+        for(int col = 1; col <= tableSize; col++){
+            for(int row = 1; row <= tableSize; row++){
+                result += String.format("%3d |", col * row);
+            }
+            result += "\n";
+        }
+        return result;
     }
 }
